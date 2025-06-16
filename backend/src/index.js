8f4a5ddce85c8365632da06f245bb044de56a0e1
Faizan Ali Khan <alikhanfaizan642@gmail.com>
2025-06-16 20:20:23 +0530

    Added changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ import path from "path";
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
@@ -28,6 +29,7 @@ app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
